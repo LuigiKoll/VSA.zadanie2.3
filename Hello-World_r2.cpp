a4886435253f1cca95e50b0639fbcfe563682d10
LuigiKoll <ljcoollmann@gmail.com>
2013-12-08 15:48:05 +0100

    r2_1st commit

---
+++ Hello-World_r2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void main(){
+	int a,b,c;
+	cout <<"Hello World !!!"<<endl<<"This is simple counter"<<endl;
+	cout<<endl<<"Zadaj cislo a:"<<endl; cin >>a;
+	cout<<endl<<"Zadaj cislo b:"<<endl; cin >>ab;
+	c=a+b;
+	cout<<a<<" + "<<b<<" = "<<c<<endl;
+	system("pause");
+}
